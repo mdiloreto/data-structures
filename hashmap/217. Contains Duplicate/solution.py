bcc12596d4b8516cc1235472a099c99a222b40c9
Mateo Di Loreto <mateoadiloreto@gmail.com>
2024-09-21 09:01:08 -0300

    update some hashmaps cases

---
+++ hashmap/217. Contains Duplicate/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        for n in nums: 
+            if nums.count(n) > 1:
+                return True
+        return False
+    
+class OtherSolution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()  # Initialize an empty set
+        for num in nums:
+            if num in seen:
+                return True  # Duplicate found
+            seen.add(num)  # Add the number to the set
+        return False  # No duplicates found
